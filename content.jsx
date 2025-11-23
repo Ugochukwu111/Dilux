ba65d4f01ec744c7bffd726b7789b2dea83d0af1
Pascal <pas111cal3@gmail.com>
2025-11-23 20:55:58 +0100

    why choose us section responsive

---
+++ content.jsx
@@ -0,0 +1,25 @@
+import { ChartNoAxesColumnDecreasing, BadgeCheck,Settings  } from 'lucide-react'
+
+export const whatWeOffer = [
+    {
+      id: 1,
+      icon: <ChartNoAxesColumnDecreasing size={30} />, 
+      name: "Data-Driven Sourcing",
+      description: "We leverage exclusive AI-powered market intelligence to identify undervalued, high-growth assets, ensuring maximum profit potential is baked into every acquisition.",
+      bgcolor:'bg-dark-maroon',
+    },
+    {
+      id: 2,
+      icon: <BadgeCheck size={30} />, 
+      name: "Rigorous 50-Point Audit",
+      description: "Every property undergoes stringent legal and structural due diligence, guaranteeing your asset is stable, fully compliant, and free from risk before a single dollar is invested.",
+      bgcolor:'bg-dark-maroon',
+    },
+    {
+      id: 3,
+      icon: <Settings  size={30} />, 
+      name: "End-to-End Asset Control",
+      description: "We manage the entire investment lifecycle in-house, eliminating third-party inefficiencies and costs, guaranteeing maximum value is returned directly to your portfolio.",
+      bgcolor:'bg-light-gold ',
+    },
+  ]
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,39 +1,60 @@
-import { MoveUpRight } from 'lucide-react'
-import { NavBar } from '../Components/NavBar'
+import { MoveUpRight } from "lucide-react";
+import { NavBar } from "../Components/NavBar";
+import { whatWeOffer } from '../../content'
 
-import './HomePage.css'
+import "./HomePage.css";
 
-
-export function HomePage(){
+export function HomePage() {
   return (
-    <div className='homepage-container'>
+    <div className="homepage-container">
       <header>
         <div className="img-overlay"></div>
-         <NavBar/>
-          <section className='hero-section-container'>
-            <h1 className='text-white'>
-              Prime Real Estate. <br />
-              <span className='text-light-gold'>
-                Guaranteed 24% Returns.
-              </span> 
-            </h1>
+        <NavBar />
+        <section className="hero-section-container">
+          <h1 className="text-white">
+            Prime Real Estate. <br />
+            <span className="text-light-gold">Guaranteed 24% Returns.</span>
+          </h1>
 
-            <p className='short-description text-light-gray'>
-              Invest with clarity and confidence. Access exclusive, fully vetted real estate opportunities, <em className='FWB text-light-gold'>legally approved </em>to deliver <em className='FWB text-light-gold'>premium lifestyle</em> rewards alongside your guaranteed <em className='FWB text-light-gold'>24%</em> annual growth.
-            </p>
+          <p className="short-description text-light-gray">
+            Invest with clarity and confidence. Access exclusive, fully vetted
+            real estate opportunities,{" "}
+            <em className="FWB text-light-gold">legally approved </em>to deliver{" "}
+            <em className="FWB text-light-gold">premium lifestyle</em> rewards
+            alongside your guaranteed{" "}
+            <em className="FWB text-light-gold">24%</em> annual growth.
+          </p>
 
-            
-      <button className='bg-white text-dark-maroon'>
-        Book Consultation Now
-        <MoveUpRight size={20} />
-      </button>
-
-          </section>
+          <button className="bg-white text-dark-maroon">
+            Book Consultation Now
+            <MoveUpRight size={20} />
+          </button>
+        </section>
       </header>
       <main>
-        <h1></h1>
+        <section className="why-choose-us-section">
+          <div className="container">
+            <h2>The Dilux Difference</h2>
+            <p className="text-dark-gray sub-text">
+              Guaranteed returns stem from our stringent, multi-layered audit.
+              We secure stable, legally compliant, exclusive assets.
+            </p>
+            <br /><br />
+            <div className="offers-container">
+              {whatWeOffer.map((offer)=>{
+                return(
+                  <div key={offer.id} className= {`offer-card ${offer.bgcolor}`}>
+                 <span className="text-white ">{offer.icon}</span>
+                 <p className="FWB offer-title text-white">{offer.name}</p>
+                 <p className="offer-description text-bright-white">{offer.description}</p>
+               </div>
+                )
+              })}
+               
+            </div>
+          </div>
+        </section>
       </main>
-       
     </div>
-  )
+  );
 }
