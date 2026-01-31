19f760e2826bb63b872b02d840615ec279e0c059
Pascal <pas111cal3@gmail.com>
2026-01-31 14:56:08 +0100

    fix: scroll react issue
    fixed the react issue that when a user scrolls down clicking the link to go to another page doesn't start from the top of that page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import ScrollToTop from "./Components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <HelmetProvider>
     <BrowserRouter>
+    <ScrollToTop/>
       <App />
     </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 );
